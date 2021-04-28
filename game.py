f3395e6ba26bc392b10e25040df5ecdd56833c7e
Ryan-Newell <55711643+Ryan-Newell@users.noreply.github.com>
2021-04-27 21:49:31 -0500

    final tests

--- game.py
+++ game.py
@@ -14,23 +14,31 @@ import json
 
 #%%
 
-# state = State(get_actual_card_pool())
-# # state2 = State(get_actual_card_pool())
-# def rungame():
-#     # p2 = RulePlayer('Player 1', 'plus')
-#     # p1 = RulePlayer('Player 2', 'plus')
-#     # p2 = RandomPlayer('Player 2')
-#     # p1.model_dict = json.loads(open('./models/model1.json').read())
-#     # p2 = RandomPlayer('Player 2')
-#     p1 = MinimaxPlayer.MinimaxPlayer('Player 1')
-#     # p1 = MCTSPlayer.MCTSPlayer('Player 1', 300000)
-#     # p2 = RandomPlayer('Player 2')
-#     p2 = HumanPlayer('Player1')
-#     # p3 = HumanPlayer('Player1')
-#
-#
-#
-#     state.play_games(8, output_result=True)
+state = State(get_actual_card_pool())
+state2 = State(get_actual_card_pool())
+def rungame():
+    # p2 = RulePlayer('Player 1', 'plus')
+    p1 = RulePlayer('Player 2', 'plus')
+    # p2 = QPlayer('Player 2', learning_rate=0.6, discount_factor=0.9)
+    p2 = ApproxQPlayer.ApproxQPlayer('Player 2')
+    # p2 = RandomPlayer('Player 2')
+    # p1.model_dict = json.loads(open('./models/model1.json').read())
+    # p2 = RandomPlayer('Player 2')
+    # p1 = MinimaxPlayer.MinimaxPlayer('Player 1')
+    # p1 = MCTSPlayer.MCTSPlayer('Player 1', 300000)
+    # p2 = RandomPlayer('Player 2')
+    p3 = HumanPlayer('Player1')
+    # p3 = HumanPlayer('Player1')
+    state.add_player(p1)
+    state.add_player(p2)
+
+
+    state.play_games(5000, output_result=False)
+
+    state2.add_player(p2)
+    state2.add_player(p3)
+    print("ready to rumble!")
+    state2.play_games(5, output_result=True)
 #
 #
 #
@@ -47,23 +55,23 @@ import json
 # print(all_results)
 
 
-x = [0, 0, 0, 0, 0]
-y = [0, 0, 0, 0, 0]
-for i in range(5):
-    state = State(get_actual_card_pool())
-    p1 = RulePlayer('Player 1')
-    p2 = QPlayer('Player 2', learning_rate=0.9, discount_factor=0.9)
-    state.add_player(p1)
-    state.add_player(p2)
-    df = pd.DataFrame()
-    all_results = []
-
-    state.play_games(num_of_games=50000, round_per_game=1, output_result=False)
-    all_results.append(state.stats)
-    [[x[i], y[i]]] = all_results
-
-print(x, y)
-print(sum(x)/len(x), sum(y)/len(y))
+# x = [0, 0, 0, 0, 0]
+# y = [0, 0, 0, 0, 0]
+# for i in range(5):
+#     state = State(get_actual_card_pool())
+#     p1 = RulePlayer('Player 1')
+#     p2 = QPlayer('Player 2', learning_rate=0.1, discount_factor=0.01)
+#     state.add_player(p1)
+#     state.add_player(p2)
+#     df = pd.DataFrame()
+#     all_results = []
+#
+#     state.play_games(num_of_games=50000, round_per_game=1, output_result=False)
+#     all_results.append(state.stats)
+#     [[x[i], y[i]]] = all_results
+#
+# print(x, y)
+# print(sum(x)/len(x), sum(y)/len(y))
 
 
 
@@ -102,5 +110,5 @@ print(sum(x)/len(x), sum(y)/len(y))
 # c1 = [4,0,1,6]
 # c2 = [0,7,6,2]
 # print(manualscore(c1, c2))
-# rungame()
+rungame()
 
